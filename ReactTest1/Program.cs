5eae0520b080902639d4750f52cd753c8446ea16
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2015-11-07 20:42:59 +0000

    Initial commit

---
+++ ReactTest1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.Owin.Hosting;
+
+namespace ReactTest1
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            using (WebApp.Start<Startup>("http://localhost:3344"))
+            {
+                Console.WriteLine("Press a key to quit...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ ReactTest1/Startup.cs
@@ -0,0 +1,38 @@
+﻿using System.Web.Http;
+using Microsoft.AspNet.SignalR;
+using Microsoft.Owin;
+using Microsoft.Owin.FileSystems;
+using Microsoft.Owin.StaticFiles;
+using Owin;
+
+[assembly: OwinStartup(typeof(ReactTest1.Startup))]
+
+namespace ReactTest1
+{
+    internal class Startup
+    {
+        // ReSharper disable once UnusedMember.Global
+        public void Configuration(IAppBuilder app)
+        {
+            // Configure serving of static files
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileSystem = new PhysicalFileSystem(@".\Client")
+            });
+
+            // Configure SignalR
+            app.MapHubs(new HubConfiguration
+            {
+                EnableCrossDomain = true,
+                EnableDetailedErrors = true
+            });
+
+            // Configure ASP.NET Web API 2
+            app.UseWebApi(new HttpConfiguration
+            // ReSharper disable once RedundantEmptyObjectOrCollectionInitializer
+            {
+                // TODO: add configuration...
+            });
+        }
+    }
+}
