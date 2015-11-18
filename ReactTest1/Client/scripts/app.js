b3e18ec9392db15cc037a111235b8459dd2ffbdd
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2015-11-18 20:25:03 +0000

    Added alert and output message areas and implemented SignalR connect/disconnect functionality

--- ReactTest1/Client/scripts/app.js
+++ ReactTest1/Client/scripts/app.js
@@ -1,19 +1,50 @@
-﻿(function() {
+﻿(function(_) {
 
     "use strict";
 
-    var hubConnection = $.hubConnection();
-    var hubProxy = hubConnection.createHubProxy("testHub");
+    var addMessage = function ($element, newline, message) {
+        var existingContent = $element.html();
+        var separator = existingContent.length > 0 ? newline : "";
+        $element.html(existingContent + separator + message);
+    };
 
-    hubProxy.on("ping", function(n) {
-        console.log("ping", n);
-    });
+    $(document).ready(function () {
 
-    hubConnection.start()
-        .done(function() {
-            console.log("hubConnection done");
-        })
-        .fail(function(reason) {
-            console.log("hubConnection fail - reason:", reason);
+        var $btnConnect = $("#btnConnect");
+        var $btnDisconnect = $("#btnDisconnect");
+        var $btnClear = $("#btnClear");
+        var $alertArea = $("#alertArea");
+        var $outputArea = $("#outputArea");
+
+        var addAlertMessage = _.partial(addMessage, $alertArea, "<br />");
+        var addOutputMessage = _.partial(addMessage, $outputArea, "\n");
+
+        var hubConnection = $.hubConnection();
+        var hubProxy = hubConnection.createHubProxy("testHub");
+
+        hubProxy.on("ping", function (n) {
+            addAlertMessage("ping " + n);
         });
-}());
+
+        $btnConnect.click(function () {
+            addOutputMessage("Calling hubConnection.start()");
+            hubConnection.start()
+                .done(function () {
+                    addOutputMessage("hubConnection.start() succeeded");
+                })
+                .fail(function (reason) {
+                    addOutputMessage("hubConnection.start() failed - reason: " + reason);
+                });
+        });
+
+        $btnDisconnect.click(function() {
+            addOutputMessage("Calling hubConnection.stop()");
+            hubConnection.stop();
+        });
+
+        $btnClear.click(function() {
+            $alertArea.html("");
+            $outputArea.html("");
+        });
+    });
+}(window._));
