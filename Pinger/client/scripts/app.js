655af9acfeac46bf79ad354d68b3abed47927af1
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2015-12-02 19:22:35 +0000

    Split SignalR code out into backend.js

--- Pinger/client/scripts/app.js
+++ Pinger/client/scripts/app.js
@@ -1,4 +1,4 @@
-﻿(function(_) {
+﻿(function() {
 
     "use strict";
 
@@ -7,30 +7,6 @@
         var separator = existingContent.length > 0 ? newline : "";
         $element.html(existingContent + separator + message);
         $element.scrollTop(1E10);
-    };
-
-    var connectionStateToString = function (state) {
-        var csValues = $.signalR.connectionState;
-        switch (state) {
-            case csValues.connecting: return "Connecting";
-            case csValues.connected: return "Connected";
-            case csValues.reconnecting: return "Reconnecting";
-            case csValues.disconnected: return "Disconnected";
-            default: return "?";
-        }
-    };
-
-    var getConnectionStateFlags = function(connectionState) {
-        var csValues = $.signalR.connectionState;
-        return {
-            isConnecting: connectionState === csValues.connecting,
-            isConnected: connectionState === csValues.connected,
-            isReconnecting: connectionState === csValues.reconnecting,
-            isDisconnected: connectionState === csValues.disconnected,
-            isUnknown: _(_.keys(csValues)).all(function(key) {
-                return connectionState !== csValues[key];
-            })
-        };
     };
 
     $(document).ready(function () {
@@ -47,74 +23,35 @@
         var addAlertMessage = _.partial(addMessage, $alertArea, "<br />");
         var addOutputMessage = _.partial(addMessage, $outputArea, "\n");
 
-        var setConnectionState = function (connection, connectionState) {
+        var handleStateChanged = function (_, __, newStateName, newFlags, transportName) {
 
-            var flags = getConnectionStateFlags(connectionState);
-
-            var enableConnectionButton = flags.isDisconnected || flags.isUnknown;
+            var enableConnectionButton = newFlags.isDisconnected || newFlags.isUnknown;
             var disableConnectionButton = !enableConnectionButton;
             var disableDisconnectionButton = enableConnectionButton;
+
             $btnConnect.prop("disabled", disableConnectionButton);
             $btnDisconnect.prop("disabled", disableDisconnectionButton);
 
-            $connectionState.text(connectionStateToString(connectionState));
-            $connectionState.toggleClass("connectionGood", flags.isConnected);
-            $connectionState.toggleClass("connectionBad", flags.isDisconnected);
-            $connectionState.toggleClass("connectionWobbly", flags.isConnecting || flags.isReconnecting);
+            $connectionState.text(newStateName);
+            $connectionState.toggleClass("connectionGood", newFlags.isConnected);
+            $connectionState.toggleClass("connectionBad", newFlags.isDisconnected);
+            $connectionState.toggleClass("connectionWobbly", newFlags.isConnecting || newFlags.isReconnecting);
 
-            $transportDetails.toggle(flags.isConnected);
-            if (flags.isConnected) {
-                $transportName.text(connection.transport.name);
-            }
+            $transportDetails.toggle(newFlags.isConnected);
+            $transportName.text(transportName);
         };
 
-        var hubConnection = $.hubConnection();
-
-        hubConnection.connectionSlow(function () {
-            addOutputMessage("[connectionSlow]");
-        });
-
-        hubConnection.disconnected(function () {
-            addOutputMessage("[disconnected]");
-        });
-
-        hubConnection.error(function (errorData) {
-            addOutputMessage("[error] errorData: " + errorData);
-        });
-
-        hubConnection.reconnected(function () {
-            addOutputMessage("[reconnected]");
-        });
-
-        hubConnection.reconnecting(function () {
-            addOutputMessage("[reconnecting]");
-        });
-
-        hubConnection.stateChanged(function (states) {
-            setConnectionState(hubConnection, states.newState);
-            var oldState = connectionStateToString(states.oldState);
-            var newState = connectionStateToString(states.newState);
-            addOutputMessage("[stateChanged] oldState: " + oldState + "; newState: " + newState);
-        });
-
-        hubConnection.starting(function () {
-            addOutputMessage("[starting]");
-        });
+        var backend = window.pinger.backend();
+        backend.onStateChanged(handleStateChanged);
 
         $btnConnect.click(function () {
             addOutputMessage("Calling hubConnection.start()");
-            hubConnection.start()
-                .done(function () {
-                    addOutputMessage("hubConnection.start() succeeded");
-                })
-                .fail(function (reason) {
-                    addOutputMessage("hubConnection.start() failed - reason: " + reason);
-                });
+            backend.start();
         });
 
         $btnDisconnect.click(function() {
             addOutputMessage("Calling hubConnection.stop()");
-            hubConnection.stop();
+            backend.stop();
         });
 
         $btnClear.click(function() {
@@ -122,12 +59,12 @@
             $outputArea.html("");
         });
 
-        var hubProxy = hubConnection.createHubProxy("testHub");
-
-        hubProxy.on("ping", function (n) {
+        backend.onMethod("testHub", "ping", function (n) {
             addAlertMessage("ping " + n);
         });
 
-        setConnectionState(hubConnection);
+        // TODO: we need to kick the initial connection state
+        // setConnectionState(hubConnection);
+
     });
-}(window._));
+}());
---
+++ Pinger/client/scripts/backend.js
@@ -0,0 +1,116 @@
+﻿(function() {
+
+    "use strict";
+
+    window.pinger = window.pinger || {};
+
+    window.pinger.backend = function () {
+
+        var hubConnection = $.hubConnection();
+        var hubProxies = {};
+
+        var connectionStateToString = function (state) {
+            var cs = $.signalR.connectionState;
+            switch (state) {
+                case cs.connecting: return "Connecting";
+                case cs.connected: return "Connected";
+                case cs.reconnecting: return "Reconnecting";
+                case cs.disconnected: return "Disconnected";
+                default: return "?";
+            }
+        };
+
+        var connectionStateValues = _.values($.signalR.connectionState);
+
+        var getConnectionStateFlags = function(connectionState) {
+            var cs = $.signalR.connectionState;
+            return {
+                isConnecting: connectionState === cs.connecting,
+                isConnected: connectionState === cs.connected,
+                isReconnecting: connectionState === cs.reconnecting,
+                isDisconnected: connectionState === cs.disconnected,
+                isUnknown: !_(connectionStateValues).contains(connectionState)
+            };
+        };
+
+        hubConnection.starting(function () {
+            // addOutputMessage("[starting]");
+        });
+
+        hubConnection.connectionSlow(function () {
+            // addOutputMessage("[connectionSlow]");
+        });
+
+        hubConnection.disconnected(function () {
+            // addOutputMessage("[disconnected]");
+        });
+
+        hubConnection.reconnecting(function () {
+            // addOutputMessage("[reconnecting]");
+        });
+
+        hubConnection.reconnected(function () {
+            // addOutputMessage("[reconnected]");
+        });
+
+        hubConnection.stateChanged(function (/* states */) {
+            // setConnectionState(hubConnection, states.newState);
+            // var oldState = connectionStateToString(states.oldState);
+            // var newState = connectionStateToString(states.newState);
+            // addOutputMessage("[stateChanged] oldState: " + oldState + "; newState: " + newState);
+        });
+
+        hubConnection.error(function (/* errorData */) {
+            // addOutputMessage("[error] errorData: " + errorData);
+        });
+
+        var getHubProxy = function (hubName) {
+            var hubProxy = hubProxies[hubName];
+            if (!hubProxy) {
+                hubProxy = hubProxies[hubName] = hubConnection.createHubProxy(hubName);
+            }
+            return hubProxy;
+        };
+
+        var start = function () {
+            hubConnection.start()
+                .done(function hubConnectionDone() {
+                    // addOutputMessage("hubConnection.start() succeeded");
+                })
+                .fail(function hubConnectionFail() {
+                    // addOutputMessage("hubConnection.start() failed - reason: " + reason);
+                });
+        };
+
+        var stop = function() {
+            hubConnection.stop();
+        };
+
+        var onStateChanged = function (cb) {
+            cb = cb || function() {};
+            hubConnection.stateChanged(function(states) {
+                var oldFlags = getConnectionStateFlags(states.oldState);
+                var newFlags = getConnectionStateFlags(states.newState);
+                var oldStateName = connectionStateToString(states.oldState);
+                var newStateName = connectionStateToString(states.newState);
+                var transportName = newFlags.isConnected ? hubConnection.transport.name : "";
+                cb(oldStateName, oldFlags, newStateName, newFlags, transportName);
+            });
+        };
+
+        var onMethod = function (hubName, methodName, cb, context) {
+            var hubProxy = getHubProxy(hubName);
+            hubProxy.on(methodName, function() {
+                var thisArg = context || null;
+                cb.apply(thisArg, arguments);
+            });
+        };
+
+        return {
+            start: start,
+            stop: stop,
+            onStateChanged: onStateChanged,
+            onMethod: onMethod
+        };
+    };
+}());
