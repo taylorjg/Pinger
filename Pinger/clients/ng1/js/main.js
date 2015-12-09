3145c0dc5245e1d1c86e6fc4b5eca06a75fea023
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2015-12-09 10:04:20 +0000

    Cleaner logging of angular-hint messages

--- Pinger/clients/ng1/js/main.js
+++ Pinger/clients/ng1/js/main.js
@@ -4,7 +4,25 @@
 
     // https://github.com/angular/angular-hint/issues/81
     if (angular.hint) {
-        angular.hint.onAny(console.log.bind(console));
+        //angular.hint.onAny(console.log.bind(console));
+        angular.hint.onAny(function () {
+            if (arguments.length >= 2) {
+                var severity = arguments[1];
+                var fn;
+                switch (severity) {
+                    case 1: /* SEVERITY_ERROR */ fn = console.error; break;
+                    case 2: /* SEVERITY_WARNING */ fn = console.warn; break;
+                    case 3: /* SEVERITY_SUGGESTION */ fn = console.info; break;
+                    default: fn = console.log; break;
+                }
+                if (arguments.length === 2) {
+                    fn.call(console, arguments[0]);
+                }
+                else {
+                    fn.call(console, arguments[0], "(" + arguments[2] + ")");
+                }
+            }
+        });
     }
 
     var app = angular.module("pinger", []);
