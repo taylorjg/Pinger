20d248abd4f83ef6aeb3fe6d7802727b144abcba
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2016-02-23 22:33:52 +0000

    More work on setup of ng2ts

---
+++ Pinger/clients/ng2ts/app/main.ts
@@ -0,0 +1,4 @@
+﻿import {bootstrap} from "angular2/platform/browser";
+import {Component} from "angular2/core";
+var x = 10;
+
--- Pinger/gulpfile.js
+++ Pinger/gulpfile.js
@@ -17,18 +17,44 @@
     configurationName = configurationName.toLowerCase();
     console.log("configurationName: " + configurationName);
 
+    // ReSharper disable once NativeTypePrototypeExtending
+    Array.prototype.last = function () {
+        var len = this.length;
+        return len > 0 ? this[len - 1] : undefined;
+    }
+
     var gulp = require("gulp");
     var jshint = require("gulp-jshint");
     var sequence = require("run-sequence");
     var del = require("del");
+    var ts = require('gulp-typescript');
 
     var srcDir = "./clients";
     var distDir = "./bin/" + configurationName + "/dist";
-    var noFwClient = { name: "nofw" };
-    var ng1Client = { name: "ng1" };
-    var ng2TsClient = { name: "ng2ts", tscTask: true };
-    var ng2JsClient = { name: "ng2js" };
-    var reactClient = { name: "react" };
+    var noFwClient = {
+        name: "nofw"
+    };
+    var ng1Client = {
+        name: "ng1"
+    };
+    var ng2TsClient = {
+        name: "ng2ts",
+        ts: true,
+        libs: [
+            "node_modules/systemjs/dist/system.src.js",
+            "node_modules/reflect-metadata/Reflect.js",
+            "node_modules/angular2/bundles/angular2.dev.js",
+            "node_modules/angular2/bundles/http.dev.js",
+            "node_modules/angular2/bundles/router.dev.js",
+            "node_modules/rxjs/bundles/Rx.js"
+        ]
+    };
+    var ng2JsClient = {
+        name: "ng2js"
+    };
+    var reactClient = {
+        name: "react"
+    };
     var clients = [noFwClient, ng1Client, ng2TsClient, ng2JsClient, reactClient];
 
     gulp.task("clean", function () {
@@ -43,6 +69,7 @@
         var clientHtmlFiles = clientSrcDir + "/*.html";
         var clientTemplateFiles = clientSrcDir + "/js/**/*.html";
         var clientJsFiles = clientSrcDir + "/js/**/*.js";
+        var clientTsFiles = clientSrcDir + "/app/**/*.ts";
         var clientCssFiles = clientSrcDir + "/css/**/*.css";
         var clientLibFiles = clientSrcDir + "/lib/**/*.js";
         var clientFiles = [
@@ -65,18 +92,35 @@
             return fullTaskName;
         };
 
-        var lintTaskName = createTask("lint", [], function() {
+        createTask("lint", [], function() {
             return gulp.src(clientJsFiles)
                 .pipe(jshint())
                 .pipe(jshint.reporter("default"));
         });
 
-        var copyFilesTaskName = createTask("copyFiles", [lintTaskName], function() {
+        if (client.ts) {
+            var tsProject = ts.createProject(clientSrcDir + "/tsconfig.json");
+            createTask("ts", [taskNames.last()], function () {
+                return gulp
+                    .src(clientTsFiles)
+                    .pipe(ts(tsProject))
+                    .pipe(gulp.dest(clientDistDir + "/app"));
+            });
+        }
+
+        if (client.libs) {
+            createTask("libs", [taskNames.last()], function () {
+                return gulp.src(client.libs)
+                    .pipe(gulp.dest(clientDistDir + "/lib"));
+            });
+        }
+
+        createTask("copyFiles", [taskNames.last()], function () {
             return gulp.src(clientFiles)
                 .pipe(gulp.dest(clientDistDir));
         });
 
-        return copyFilesTaskName;
+        return taskNames.last();
     };
 
     gulp.task("copyLandingPage", function () {
