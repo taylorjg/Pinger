816d846bc651db1c6902bc9d82121e02793cbf4b
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2016-02-23 23:16:08 +0000

    Got simplest ng2 app working in ng2ts

--- Pinger/clients/ng2ts/app/main.ts
+++ Pinger/clients/ng2ts/app/main.ts
@@ -1,4 +1,6 @@
-﻿import {bootstrap} from "angular2/platform/browser";
-import {Component} from "angular2/core";
-var x = 10;
+import {bootstrap} from "angular2/platform/browser";
+import {AppComponent} from "./app.component";
+import {enableProdMode} from 'angular2/core';
 
+//enableProdMode();
+bootstrap(AppComponent);
