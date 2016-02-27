317187555066a1ef1a00dc3ae99376e3691583c2
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2016-02-27 13:27:39 +0000

    Started to wire up SignalR hub connection

--- Pinger/clients/ng2ts/app/home/signalR.service.ts
+++ Pinger/clients/ng2ts/app/home/signalR.service.ts
@@ -5,11 +5,16 @@ import {ConnectionState} from "./connectionState";
 export class SignalRService {
     @Output() stateChanged: EventEmitter<ConnectionState> = new EventEmitter();
     @Output() eventLogged: EventEmitter<string> = new EventEmitter();
+    private hubConnection = $.hubConnection();
     start() {
         console.log("SignalRService.start");
-        this.stateChanged.emit(new ConnectionState(4));
+        this.hubConnection.start(() => {
+            console.log("hubConnection.start callback");
+            this.stateChanged.emit(new ConnectionState(this.hubConnection.state));
+        });
     }
     stop() {
         console.log("SignalRService.stop");
+        this.hubConnection.stop();
     }
 }
