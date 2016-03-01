9184d3b82e34fc62033d987ce0809902afcb7c8d
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2016-03-01 20:54:33 +0000

    ng2ts - now using ngClass to apply css classes to connection state badge

--- Pinger/clients/ng2ts/app/home/signalrPanel.component.ts
+++ Pinger/clients/ng2ts/app/home/signalrPanel.component.ts
@@ -1,6 +1,7 @@
 ﻿// ReSharper disable InconsistentNaming
 
 import {Component} from "angular2/core";
+import {NgClass} from 'angular2/common';
 import {SignalRService} from "./signalR.Service";
 import {ConnectionState} from "./connectionState";
 import {ConnectionStateFlags} from "./connectionStateFlags";
@@ -10,20 +11,26 @@ import {ConnectionStateFlags} from "./connectionStateFlags";
     template: `
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
-                Connection state: <span id="connectionState" class="badge">{{ connectionState() }}</span>
+                Connection state: <span id="connectionState" class="badge" [ngClass]="connectionStateClasses">{{ connectionState() }}</span>
                 <span id="transportDetails">Transport: <span id="transportName" class="badge connectionGood">{{ transportName() }}</span></span>
                 <button type="button" id="btnConnect" class="btn btn-sm btn-primary" (click)="onConnect()">Connect</button>
                 <button type="button" id="btnDisconnect" class="btn btn-sm btn-primary" (click)="onDisconnect()">Disconnect</button>
             </div>
-        </div>`
+        </div>`,
+    directives: [NgClass]
 })
 export class SignalRPanelComponent {
     private _stateChangedSubscription = null;
     private _connectionState: ConnectionState = null;
+    connectionStateClasses = {
+        connectionGood: false,
+        connectionBad: false,
+        connectionWobbly: false
+    };
     constructor(private signalRService: SignalRService) {
     }
     connectionState(): string {
-        return this._connectionState != null ? this._connectionState.newState.toString() : "";
+        return this._connectionState != null ? this._connectionState.newState.toString() : "?";
     }
     transportName(): string {
         return this._connectionState != null ? this._connectionState.transportName : "";
@@ -38,8 +45,11 @@ export class SignalRPanelComponent {
     }
     ngOnInit() {
         console.log("SignalRPanelComponent.ngOnInit");
-        this._stateChangedSubscription = this.signalRService.stateChanged.subscribe(e => {
+        this._stateChangedSubscription = this.signalRService.stateChanged.subscribe((e: ConnectionState) => {
             this._connectionState = e;
+            this.connectionStateClasses.connectionGood = e.newStateFlags.isConnected;
+            this.connectionStateClasses.connectionBad = e.newStateFlags.isDisconnected;
+            this.connectionStateClasses.connectionWobbly = e.newStateFlags.isConnecting || e.newStateFlags.isReconnecting;
         });
     }
     ngOnDestroy() {
