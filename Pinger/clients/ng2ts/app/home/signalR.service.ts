2be178473825ff699a796926b14308b5ad6076b2
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2016-03-06 11:02:40 +0000

    ng2ts - finished wiring up all the log events in SignalRService

--- Pinger/clients/ng2ts/app/home/signalR.service.ts
+++ Pinger/clients/ng2ts/app/home/signalR.service.ts
@@ -10,8 +10,26 @@ export class SignalRService {
     @Output() logEvent: EventEmitter<string> = new EventEmitter();
     private _hubConnection = $.hubConnection();
     constructor() {
-        this._hubConnection.stateChanged(_ => {
-            this._raiseStateChanged();
+        this._hubConnection.starting(() => {
+            this._raiseLogEvent("[starting]");
+        });
+        this._hubConnection.connectionSlow(() => {
+            this._raiseLogEvent("[connectionSlow]");
+        });
+        this._hubConnection.disconnected(() => {
+            this._raiseLogEvent("[disconnected]");
+        });
+        this._hubConnection.reconnecting(() => {
+            this._raiseLogEvent("[reconnecting]");
+        });
+        this._hubConnection.reconnected(() => {
+            this._raiseLogEvent("[reconnected]");
+        });
+        this._hubConnection.stateChanged(change => {
+            this._raiseStateChanged(change);
+        });
+        this._hubConnection.error(error => {
+            this._raiseLogEvent("[error]", "error.message:", error.message);
         });
     }
     start() {
@@ -20,18 +38,24 @@ export class SignalRService {
                 this._raiseLogEvent("[start.done]");
             },
             (reason: string) => {
-                this._raiseLogEvent(["[start.fail]", "reason:", reason].join(" "));
+                this._raiseLogEvent("[start.fail]", "reason:", reason);
             });
     }
     stop() {
         this._hubConnection.stop();
     }
-    private _raiseStateChanged() {
+    private _raiseStateChanged(change: SignalRStateChange) {
+
+        if (change.oldState !== undefined) {
+            var oldStateName = ConnectionStatePipe.connectionStateToString(change.oldState);
+            var newStateName = ConnectionStatePipe.connectionStateToString(change.newState);
+            this._raiseLogEvent("[stateChanged]", "oldState:", oldStateName, "newState:", newStateName);
+        }
+
         var connectionState = new ConnectionState(this._hubConnection);
-        this._raiseLogEvent(["newState", ConnectionStatePipe.connectionStateToString(connectionState.newState)].join(" "));
         this.stateChanged.emit(connectionState);
     }
-    private _raiseLogEvent(s: string) {
-        this.logEvent.emit(s);
+    private _raiseLogEvent(...args) {
+        this.logEvent.emit(args.join(" "));
     }
 }
