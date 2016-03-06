54764b5bce4f981528909667e6e45cfe592edb87
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2016-03-06 23:37:39 +0000

    ng2ts - alertArea and outputArea now scroll to bottom when a message is added

--- Pinger/clients/ng2ts/app/home/alertArea.component.ts
+++ Pinger/clients/ng2ts/app/home/alertArea.component.ts
@@ -1,12 +1,12 @@
 ﻿// ReSharper disable InconsistentNaming
 
-import {Component} from "angular2/core";
+import {Component, ViewChild} from "angular2/core";
 
 @Component({
     selector: "alert-area",
     template: `
         <div class="row">
-            <div class="col-md-7 col-md-offset-2 alert alert-success scrollableMessageArea">
+            <div class="col-md-7 col-md-offset-2 alert alert-success scrollableMessageArea" #scrollableMessageArea>
                 <div *ngFor="#message of _messages">{{ message }}</div>
             </div>
             <div class="col-md-1">
@@ -17,9 +17,11 @@ import {Component} from "angular2/core";
         </div>`
 })
 export class AlertAreaComponent {
+    @ViewChild("scrollableMessageArea") private scrollableMessageArea;
     private _messages: string[] = [];
     addMessage(message: string) {
         this._messages.push(message);
+        this.scrollableMessageArea.nativeElement.scrollTop = 1E10;
     }
     onClear() {
         this._messages = [];
--- Pinger/clients/ng2ts/app/home/outputArea.component.ts
+++ Pinger/clients/ng2ts/app/home/outputArea.component.ts
@@ -1,12 +1,12 @@
 ﻿// ReSharper disable InconsistentNaming
 
-import {Component} from "angular2/core";
+import {Component, ViewChild} from "angular2/core";
 
 @Component({
     selector: "output-area",
     template: `
         <div class="row">
-            <pre class="col-md-7 col-md-offset-2 scrollableMessageArea">
+            <pre class="col-md-7 col-md-offset-2 scrollableMessageArea" #scrollableMessageArea>
                 <div *ngFor="#message of _messages">{{ message }}</div>
             </pre>
             <div class="col-md-1">
@@ -17,9 +17,11 @@ import {Component} from "angular2/core";
         </div>`
 })
 export class OutputAreaComponent {
+    @ViewChild("scrollableMessageArea") private scrollableMessageArea;
     private _messages: string[] = [];
     addMessage(message: string) {
         this._messages.push(message);
+        this.scrollableMessageArea.nativeElement.scrollTop = 1E10;
     }
     onClear() {
         this._messages = [];
